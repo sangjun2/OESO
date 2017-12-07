a995fb8c663cdcd6a886b66c63a3448392ae3230
JeonSeongBae <tjdqo6042@naver.com>
2017-12-08 00:11:44 +0900

    database read

--- OnU/app/src/main/java/com/foxyawn/onu/MainFragment_pro.java
+++ OnU/app/src/main/java/com/foxyawn/onu/MainFragment_pro.java
@@ -4,11 +4,17 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
 
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 /**
@@ -29,6 +35,7 @@ public class MainFragment_pro extends Fragment {
     private String mParam1;
     private String mParam2;
     private OnFragmentInteractionListener mListener;
+    FirebaseUser user;
 
     public MainFragment_pro() {
         // Required empty public constructor
@@ -61,6 +68,19 @@ public class MainFragment_pro extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+        FirebaseDatabase.getInstance().getReference().child("users").child("consumer").child("PQncueXU2yXAojQHFroSA3xy9d22").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    Log.d("MainActivity", "ValueEventListener : " + snapshot.getValue());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
     }
 
